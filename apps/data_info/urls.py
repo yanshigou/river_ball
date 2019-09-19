142b9fceb7797083a62ba133c9fa3520d483d8ea
cmx_company <569578851@qq.com>
2019-09-19 18:08:40 +0800

    url onenet传输

--- apps/data_info/urls.py
+++ apps/data_info/urls.py
@@ -3,13 +3,14 @@ __author__ = "dzt"
 __date__ = "2019/9/4"
 from django.conf.urls import url
 from .views import LocationInfoView, SelectDevice, UploadLocationInfoView, StatisticalToOneView, LocationTrackView
-from .views import UploadTXT, Uploaded, ShowTXTView, DelFile
+from .views import ShowTXTView, OneNetDataView
 
 
 urlpatterns = [
     url(r'^selectDevice/$', SelectDevice.as_view(), name='select_device'),
     url(r'^locationInfo/(?P<imei_id>\d+)/$', LocationInfoView.as_view(), name='location_info'),
     url(r'^upload/$', UploadLocationInfoView.as_view()),
+    url(r'^rawData/$', OneNetDataView.as_view()),
     url(r'^statistical/(?P<imei_id>\d+)/$', StatisticalToOneView.as_view(), name='statistical'),
     url(r'^track/(?P<imei_id>\d+)/(?P<start_time>.*)/(?P<end_time>.*)$', LocationTrackView.as_view(), name='track'),
     # url(r'^uploadFile/$', UploadTXT.as_view(), name='uploadTXT'),
