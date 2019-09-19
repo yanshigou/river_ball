90d0730dab5d5405ee528fade4a9a41f1d9e0051
cmx_company <569578851@qq.com>
2019-09-19 18:07:49 +0800

    新增onenet表 修改除时间经纬度速度外可为空

--- apps/data_info/models.py
+++ apps/data_info/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from devices.models import DevicesInfo
 
 
+class DevicesOneNetInfo(models.Model):
+    dev_id = models.CharField(max_length=30, verbose_name='OneNetID')
+    imei = models.ForeignKey(DevicesInfo, verbose_name='IMEI')
+
+
 # 原始数据
 class LocationInfo(models.Model):
     imei = models.ForeignKey(DevicesInfo, verbose_name='设备号')
@@ -10,13 +15,15 @@ class LocationInfo(models.Model):
     get_time = models.DateTimeField(verbose_name='收到时间', auto_now_add=True)
     longitude = models.CharField(max_length=30, verbose_name='经度')
     latitude = models.CharField(max_length=30, verbose_name='纬度')
-    altitude = models.CharField(max_length=10, verbose_name='海拔米')
+    altitude = models.CharField(max_length=10, verbose_name='海拔米', null=True, blank=True)
     speed = models.CharField(max_length=10, verbose_name='速度公里/小时')
-    direction = models.CharField(max_length=10, verbose_name='方向')
-    accuracy = models.CharField(max_length=10, verbose_name='测量精度')
-    power = models.CharField(max_length=10, verbose_name='剩余电量')
-    satellites = models.CharField(max_length=10, verbose_name='搜星数量')
-    real_satellites = models.CharField(max_length=10, verbose_name='实际使用卫星数量')
+    direction = models.CharField(max_length=10, verbose_name='方向', null=True, blank=True)
+    accuracy = models.CharField(max_length=10, verbose_name='测量精度', null=True, blank=True)
+    power = models.CharField(max_length=10, verbose_name='剩余电量', null=True, blank=True)
+    satellites = models.CharField(max_length=10, verbose_name='搜星数量', null=True, blank=True)
+    real_satellites = models.CharField(max_length=10, verbose_name='实际使用卫星数量', null=True, blank=True)
+    EW_hemisphere = models.CharField(max_length=2, verbose_name='东西半球', null=True, blank=True)
+    NS_hemisphere = models.CharField(max_length=2, verbose_name='南北半球', null=True, blank=True)
 
     class Meta:
         verbose_name = "原始数据信息表"
